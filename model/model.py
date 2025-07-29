fix concavity calculation

model/model.py
@@ -5,144 +5,8 @@ import torch.nn.functional as F
 from model.utils.vn_layers import *
 from model.utils.vn_pointnet import PointNetEncoder
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from pointnet2_ops.pointnet2_modules import PointnetFPModule#, PointnetSAModuleMSG
+from pointnet2_ops.pointnet2_modules import PointnetFPModule, PointnetSAModuleMSG
 from torch.optim.lr_scheduler import LambdaLR
-
-
-from typing import List, Optional, Tuple
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from pointnet2_ops import pointnet2_utils  
-
-def build_shared_mlp(mlp_spec: List[int], bn: bool = True):
-    layers = []
-    for i in range(1, len(mlp_spec)):
-        layers.append(
-            nn.Conv2d(mlp_spec[i - 1], mlp_spec[i], kernel_size=1, bias=not bn)
-        )
-        if bn:
-            layers.append(nn.BatchNorm2d(mlp_spec[i]))
-        layers.append(nn.ReLU(True))
-
-    return nn.Sequential(*layers)
-
-import pointnet2_ops._ext as _ext
-def debug_radius_coverage(new_xyz, xyz, radius, nsample):
-    B, N, _ = xyz.shape
-    neighbors = _ext.ball_query(new_xyz, xyz, radius, nsample)
-    print(f"Radius {radius}, NSample {nsample}:")
-    for b in range(B):
-        for i in range(new_xyz.shape[1]):
-            num_neighbors = (neighbors[b, i] >= 0).sum().item()
-            for neighbor in neighbors[b, i]:
-                if neighbor >= 0:
-                    dist2 = torch.sqrt(torch.sum((xyz[b, neighbor] - new_xyz[b, i]) ** 2))
-                    print(f"  Point {i} in batch {b} has {num_neighbors} neighbors, distance: {dist2.item()}")
-
-class _PointnetSAModuleBase(nn.Module):
-    def __init__(self):
-        super(_PointnetSAModuleBase, self).__init__()
-        self.npoint = None
-        self.groupers = None
-        self.mlps = None
-
-    def forward(
-        self, xyz: torch.Tensor, features: Optional[torch.Tensor]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        r"""
-        Parameters
-        ----------
-        xyz : torch.Tensor
-            (B, N, 3) tensor of the xyz coordinates of the features
-        features : torch.Tensor
-            (B, C, N) tensor of the descriptors of the the features
-
-        Returns
-        -------
-        new_xyz : torch.Tensor
-            (B, npoint, 3) tensor of the new features' xyz
-        new_features : torch.Tensor
-            (B,  \sum_k(mlps[k][-1]), npoint) tensor of the new_features descriptors
-        """
-
-        new_features_list = []
-
-        xyz_flipped = xyz.transpose(1, 2).contiguous()
-        new_xyz = (
-            pointnet2_utils.gather_operation(
-                xyz_flipped, pointnet2_utils.furthest_point_sample(xyz, self.npoint)
-            )
-            .transpose(1, 2)
-            .contiguous()
-            if self.npoint is not None
-            else None
-        )
-
-        for i in range(len(self.groupers)):
-            new_features = self.groupers[i](
-                xyz, new_xyz, features
-            )  # (B, C, npoint, nsample)
-
-            # if self.groupers[i].radius == 0.1:
-            #     print(features.shape)
-            #     print(new_features.shape)
-            #     for i in range(5):
-            #         print(new_features[0, :3, i, :])
-            #     exit()
-
-            new_features = self.mlps[i](new_features)  # (B, mlp[-1], npoint, nsample)
-            new_features = F.max_pool2d(
-                new_features, kernel_size=[1, new_features.size(3)]
-            )  # (B, mlp[-1], npoint, 1)
-            new_features = new_features.squeeze(-1)  # (B, mlp[-1], npoint)
-
-            new_features_list.append(new_features)
-
-        return new_xyz, torch.cat(new_features_list, dim=1)
-
-
-
-class PointnetSAModuleMSG(_PointnetSAModuleBase):
-    r"""Pointnet set abstrction layer with multiscale grouping
-
-    Parameters
-    ----------
-    npoint : int
-        Number of features
-    radii : list of float32
-        list of radii to group with
-    nsamples : list of int32
-        Number of samples in each ball query
-    mlps : list of list of int32
-        Spec of the pointnet before the global max_pool for each scale
-    bn : bool
-        Use batchnorm
-    """
-
-    def __init__(self, npoint, radii, nsamples, mlps, bn=True, use_xyz=True):
-        # type: (PointnetSAModuleMSG, int, List[float], List[int], List[List[int]], bool, bool) -> None
-        super(PointnetSAModuleMSG, self).__init__()
-
-        assert len(radii) == len(nsamples) == len(mlps)
-
-        self.npoint = npoint
-        self.groupers = nn.ModuleList()
-        self.mlps = nn.ModuleList()
-        for i in range(len(radii)):
-            radius = radii[i]
-            nsample = nsamples[i]
-            self.groupers.append(
-                pointnet2_utils.QueryAndGroup(radius, nsample, use_xyz=use_xyz)
-                if npoint is not None
-                else pointnet2_utils.GroupAll(use_xyz)
-            )
-            mlp_spec = mlps[i]
-            if use_xyz:
-                mlp_spec[0] += 3
-
-            self.mlps.append(build_shared_mlp(mlp_spec, bn))
 
 
 class ACDModel(pl.LightningModule):
@@ -163,7 +27,7 @@ class ACDModel(pl.LightningModule):
         self.SA_modules.append(
             PointnetSAModuleMSG(
                 npoint=10000,
-                radii=[0.02, 0.03, 0.05],
+                radii=[0.03, 0.04, 0.05],
                 nsamples=[8 ,16, 32],
                 mlps=[[c_in, 8, 8, 16], [c_in, 16, 16, 32], [c_in, 32, 32, 64]],
                 use_xyz=self.hparams.use_xyz,
@@ -175,7 +39,7 @@ class ACDModel(pl.LightningModule):
         self.SA_modules.append(
             PointnetSAModuleMSG(
                 npoint=2500,
-                radii=[0.05, 0.1],
+                radii=[0.1, 0.2],
                 nsamples=[16, 32],
                 mlps=[[c_in, 64, 64, 128], [c_in, 64, 96, 128]],
                 use_xyz=self.hparams.use_xyz,
@@ -187,7 +51,7 @@ class ACDModel(pl.LightningModule):
         self.SA_modules.append(
             PointnetSAModuleMSG(
                 npoint=512,
-                radii=[0.1, 0.25],
+                radii=[0.3, 0.4],
                 nsamples=[16, 32],
                 mlps=[[c_in, 128, 196, 256], [c_in, 128, 196, 256]],
                 use_xyz=self.hparams.use_xyz,

utils/ACDgen.py
@@ -48,7 +48,7 @@ class ACDgen(IterableDataset):
 
         pcd.points = o3d.utility.Vector3dVector(smoothed_points)
 
-    def apply_random_scale(self, pcd):
+    def apply_random_scale(self, pcd, structure):
         scale_x = np.random.uniform(0.5, 4.0)
         scale_y = np.random.uniform(0.5, 4.0)
         scale_z = np.random.uniform(0.5, 4.0)
@@ -63,8 +63,10 @@ class ACDgen(IterableDataset):
         points = points @ stretch_matrix.T
 
         pcd.points = o3d.utility.Vector3dVector(points)
+
+        structure.vertices = structure.vertices @ stretch_matrix.T
     
-    def apply_random_rotation(self, pcd):
+    def apply_random_rotation(self, pcd, structure):
         angle = np.random.uniform(0, 2 * np.pi)
         rotation_matrix = np.array([
             [np.cos(angle), -np.sin(angle), 0],
@@ -77,10 +79,13 @@ class ACDgen(IterableDataset):
 
         pcd.points = o3d.utility.Vector3dVector(points)
 
-    def normalize_mesh(self, points):
+        structure.vertices = structure.vertices @ rotation_matrix.T
+
+    def normalize_mesh(self, points, structure):
         min_vals = points.min(axis=0)
         max_vals = points.max(axis=0)
         points = (points - min_vals) / (max_vals - min_vals)
+        structure.vertices = lib_neural_acd.VecArray3d((np.asarray(structure.vertices) - min_vals) / (max_vals - min_vals))
         return points
             
     def __iter__(self):
@@ -101,19 +106,22 @@ class ACDgen(IterableDataset):
             distances = self.get_distances(np.asarray(pcd.points), structure.cut_verts)
 
             if self.config.generation.random_scale:
-                self.apply_random_scale(pcd)
+                self.apply_random_scale(pcd, structure)
             if self.config.generation.random_rotation:
-                self.apply_random_rotation(pcd)
+                self.apply_random_rotation(pcd,structure)
             if self.config.generation.gaussian_filter:
                 self.apply_gaussian_filter(pcd,sigma=0.1,radius=random.uniform(0.01, 0.15))
             
             points = np.asarray(pcd.points)
 
+            
             if self.config.generation.normalize_mesh:
-                points = self.normalize_mesh(points)
+                points = self.normalize_mesh(points,structure)
 
             mesh = trimesh.Trimesh(vertices=structure.vertices, faces=structure.triangles)
             curvature = trimesh.curvature.discrete_gaussian_curvature_measure(mesh, points, 0.02)
+
+
 
             #add curvature channel
             points = np.hstack((points, curvature[:, np.newaxis]))